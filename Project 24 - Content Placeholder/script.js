06411e670f19c1e8b56734acffeb6bea919a0e78
Kimberly Palmer <kimberlylpalmer@gmail.com>
2025-04-24 14:31:32 -0700

    Sticky Navigation Bar

--- Project 24 - Content Placeholder/script.js
+++ Project 24 - Content Placeholder/script.js
@@ -8,10 +8,10 @@ const date = document.getElementById("date");
 const animated_bgs = document.querySelectorAll(".animated-bg");
 const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
 
-setTimeout(getData, 2500);
+setTimeout(getData, 7500);
 
 function getData() {
-  header.innerHTML = 
+  header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1743885143645-b28cdaacf8b5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />';
   title.innerHTML = "Lorem ipsum dolor sit amet.";
   excerpt.innerHTML =
